e0f10549be1897e0c64fc48ba3ee896ece69b378
Alina <malinka775@yandex.ru>
2022-05-02 23:43:32 +0300

    feat: solve(almost) date task

--- src/what-season.js
+++ src/what-season.js
@@ -12,39 +12,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  let myDate = new Date(date)
-  let month = myDate.getMonth()
-
-  if (typeof(month) !== 'number' || myDate === 'Invalid date!') {
+function getSeason( date ) {
+  //throw new NotImplementedError('Not implemented');
+  if (!date) {
     return 'Unable to determine the time of year!'
-  }
-
-  if (isNaN(Date.parse(date)) ) {
+  } 
+  if (date instanceof Date && date.getTime && typeof(date.getTime) === 'function') {
+    if(Object.prototype.toString.call(date) === '[object Date]') {
+     
+        let month = date.getMonth()
+        switch (month){
+          case 11:
+            case 0:
+              case 1: return 'winter'
+          case 2:
+            case 3:
+              case 4: return 'spring'
+          case 5:
+            case 6:
+              case 7: return 'summer'
+          case 8:
+            case 9:
+              case 10: return 'autumn'
+      } 
+      throw new Error('Invalid date!')
+    }
+    
+  } else {
     throw new Error('Invalid date!')
   }
- 
- 
-  //;
-  
-
-  switch (month){
-    case 11:
-      case 0:
-        case 1: return 'winter'
-    case 2:
-      case 3:
-        case 4: return 'spring'
-    case 5:
-      case 6:
-        case 7: return 'summer'
-    case 8:
-      case 9:
-        case 10: return 'autumn'
-  }
-  
-  
 }
 
 
